f81a1db89660b70b2bf5d74f72ce6cc6f04adbb4
Joseph Falco <falco.joseph10155@gmail.com>
2017-10-29 17:38:03 -0400

    Added models to admin website

--- Noatpad/track_n_drive/admin.py
+++ Noatpad/track_n_drive/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User
+from .models import Car, Phone, PhoneTimings, Email, EmailTimings, Settings, TechAddedInfo, Technician, Repair, UserAddedInfo, Notifications, License, Insurance, User
 
+admin.site.register(Car)
+admin.site.register(Phone)
+admin.site.register(PhoneTimings)
+admin.site.register(Email)
+admin.site.register(EmailTimings)
+admin.site.register(Settings)
+admin.site.register(TechAddedInfo)
+admin.site.register(Technician)
+admin.site.register(Repair)
+admin.site.register(UserAddedInfo)
+admin.site.register(Notifications)
+admin.site.register(License)
+admin.site.register(Insurance)
 admin.site.register(User)
