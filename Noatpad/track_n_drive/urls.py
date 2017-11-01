320486f12ebff4a90631dfe9907169c822ea4d0a
Ian Torres <itorres@umass.edu>
2017-11-01 16:51:37 -0400

    changed url: track_n_drive/url.py

--- Noatpad/track_n_drive/urls.py
+++ Noatpad/track_n_drive/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^car-prof-(?P<id>)/$', views.car_prof, name='carprof'),
+    url(r'^car-prof-(?P<id>)/$', views.car_prof, name='car'),
 ]
