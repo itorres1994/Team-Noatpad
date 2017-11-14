3b4876ba76640d35ff973054931d7b0f12892338
Corey Schotte <caschott@umass.edu>
2017-11-14 16:27:51 -0500

    Fleshed out the Forms in forms.py

--- Noatpad/track_n_drive/forms.py
+++ Noatpad/track_n_drive/forms.py
@@ -1,15 +1,54 @@
-from django import ModelForms
+from django.forms import ModelForm
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 import datetime
 
-from .models import Technician, TechAddedInfo, Car, FutureRepair, Repair, Phone, Email, UserAddedInfo
+from .models import Technician, TechAddedInfo, Car, FutureRepair, Repair, Phone, Email,  \
+    UserAddedInfo, EmailTimings, PhoneTimings
 
 class AddTechnicianForm(ModelForm):
     class Meta:
         model = Technician
-        fields = ['fname', 'lname',]
+        exclude = { 'unique_id' }
         labels = { 'fname': _('First Name'), 'lname': _('Last Name'),}
-        help_texts = { 'fname': _('Enter Technician first name.'),}
+
+class AddTechAddedInfoForm(ModelForm):
+    class Meta:
+        model = TechAddedInfo
+
+class AddCarForm(ModelForm):
+    class Meta:
+        model = Car
+        exclude = { 'unique_id' }
+
+class AddFutureRepairForm(ModelForm):
+    class Meta:
+        model = FutureRepair
+        exclude = { 'unique_id' }
+
+class AddRepairForm(ModelForm):
+    class Meta:
+        model = Repair
+        exclude = { 'unique_id' }
+
+class AddPhoneForm(ModelForm):
+    class Meta:
+        model = Phone
+
+class AddEmailForm(ModelForm):
+    class Meta:
+        model = Email
+
+class AddUserAddedInfoForm(ModelForm):
+    class Meta:
+        model = UserAddedInfo
+
+# class AddPhoneTimingsForm(ModelForm):
+#     class Meta:
+#         model = PhoneTimings
+#
+# class AddEmailTimingsForm(ModelForm):
+#     class Meta:
+#         model = EmailTimings
